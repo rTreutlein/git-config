d0f2ff70b806f7e6955a13ab63c90e09d3b8855c
Roman Treutlein <roman.treutlein@gmail.com>
2023-05-03 09:19:01 +0200

    Simplify initNvim.sh

--- initNvim.sh
+++ initNvim.sh
@@ -3,12 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Creating nvim Config Folders"
-mkdir ~/.config/nvim
 echo "Linking Vim Config"
-ln -s "$DIR/nvim/init.lua" ~/.config/nvim/init.lua
-echo "Linking vim lua folder"
-ln -s "$DIR/nvim/lua" ~/.config/nvim/lua
-echo "Linking vim after folder"
-ln -s "$DIR/nvim/after" ~/.config/nvim/after
+ln -s "$DIR/nvim" ~/.config/nvim/
 echo "Linking Tmux Config"
 ln -s "$DIR/tmux.conf" ~/.tmux.conf
