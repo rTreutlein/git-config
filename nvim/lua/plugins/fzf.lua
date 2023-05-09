dedfb3926cafa2e2d27aa7d15ca39f3219b1b3e5
Roman Treutlein <roman.treutlein@gmail.com>
2023-05-09 06:23:55 +0200

    Switch from fzf to telescope for fileexplorer

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,6 +1,7 @@
 return {
     'ibhagwan/fzf-lua',
     dependencies = { 'junegunn/fzf', build = './install --bin', },
+    enabled = false,
     keys = {
         { "<leader>o", "<cmd>lua require('fzf-lua').files()<CR>", mode = "n", desc = "FZF open file", silent = true },
         { "<leader>f", "<cmd>lua require('fzf-lua').live_grep()<CR>", mode = "n", desc = "FZF live grep", silent = true  },
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        --cmd = "Telescope",
+        --enabled = false,
+        opts = {
+            pickers = {
+                find_files = {
+                    follow = true,
+                }
+            }
+        },
+        keys = {
+            { "<leader>o", function() require('telescope.builtin').find_files() end, mode = "n", desc = "Telescope open file", silent = true },
+            { "<leader>f", function() require('telescope.builtin').live_grep() end, mode = "n", desc = "Telescope live grep", silent = true  },
+            { "<leader>b", function() require('telescope.builtin').buffers() end, mode = "n", desc = "Telescope buffers", silent = true  },
+        },
+    },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        keys = {
+            { "<leader>z", function() require('telescope').extensions.file_browser.file_browser() end, mode = "n", desc = "Telescope open file", silent = true },
+        }
+    }
+}
