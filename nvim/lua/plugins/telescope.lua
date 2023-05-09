3da3c9e76b13535504cf2a667940def1e3ff6c8e
Roman Treutlein <roman.treutlein@iis.fraunhofer.de>
2023-05-09 07:10:50 +0200

    Telescope find hidden fiesl

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
             pickers = {
                 find_files = {
                     follow = true,
+                    hidden = true,
                 }
             }
         },
