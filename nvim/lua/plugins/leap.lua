7660db266e3a6a545459f206e3b02ff322181727
Roman Treutlein <roman.treutlein@gmail.com>
2023-05-30 09:44:36 +0200

    Fix leap config

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,6 +1,8 @@
 return {
     'ggandor/leap.nvim', --Nvims answer to the mouse
-    confgi = function (plug)
-        plug.add_default_mappings()
+    config = function ()
+        local leap = require'leap'
+        leap.add_default_mappings()
+        vim.api.nvim_set_hl(0, 'LeapBackdrop', { fg = '#777777' })
     end
 }
