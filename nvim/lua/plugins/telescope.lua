d2cae7940b7f83431a80df0fbb6a6030a7346cc9
Roman Treutlein <roman@trueagi.io>
2025-03-29 15:09:53 +0100

    enable telescop but no keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         --cmd = "Telescope",
-        enabled = false,
+        --enabled = false,
         opts = {
             pickers = {
                 find_files = {
@@ -12,14 +12,15 @@ return {
                 }
             }
         },
-        keys = {
-            { "<leader>o", function() require('telescope.builtin').find_files() end, mode = "n", desc = "Telescope open file", silent = true },
-            { "<leader>f", function() require('telescope.builtin').live_grep() end, mode = "n", desc = "Telescope live grep", silent = true  },
-            { "<leader>b", function() require('telescope.builtin').buffers() end, mode = "n", desc = "Telescope buffers", silent = true  },
-        },
+        --keys = {
+        --    { "<leader>o", function() require('telescope.builtin').find_files() end, mode = "n", desc = "Telescope open file", silent = true },
+        --    { "<leader>f", function() require('telescope.builtin').live_grep() end, mode = "n", desc = "Telescope live grep", silent = true  },
+        --    { "<leader>b", function() require('telescope.builtin').buffers() end, mode = "n", desc = "Telescope buffers", silent = true  },
+        --},
     },
     {
         "nvim-telescope/telescope-file-browser.nvim",
+        enabled = false,
         keys = {
             { "<leader>z", function() require('telescope').extensions.file_browser.file_browser() end, mode = "n", desc = "Telescope open file", silent = true },
         }
