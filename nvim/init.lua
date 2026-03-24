319f1a125841266916717b64804e20947d1ea405
Roman Treutlein <roman@trueagi.io>
2026-03-24 12:19:35 +0100

    fixup

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,14 +27,12 @@ vim.cmd.colorscheme "catppuccin-mocha"
 local lazygit = require'FTerm':new{cmd = "lazygit"}
 vim.keymap.set('n', '<leader>gg', function() lazygit:toggle() end)
 
-
 ---
 -- Setup haskell LSP
 ---
 
 vim.g.haskell_tools = {
   hls = {
-    capabilities = require('cmp_nvim_lsp').default_capabilities(),
     on_attach = function(client, bufnr, ht)
     print("Starting haskell-lsp")
     ---
@@ -60,3 +58,33 @@ vim.g.haskell_tools = {
     end,
   }
 }
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function (event)
+    -- print("Attached")
+    local opts = {buffer = event.buf}
+    vim.keymap.set('n', '<leader>a', function ()
+      require('fzf-lua').lsp_code_actions{}
+    end, opts)
+    vim.keymap.set('v', '<leader>a', function ()
+      require('fzf-lua').lsp_code_actions{}
+    end, opts)
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})
+
+vim.keymap.set('n','<leader>ss', ':ClangdSwitchSourceHeader<CR>')
+
+vim.lsp.enable({'clangd', 'basedpyright', 'luals', 'metta_lsp'})
